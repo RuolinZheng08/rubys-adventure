c5e72c308d96b32e020a8ee544c1a01301a7eef5
Lynn Zheng <ruolinzheng@uchicago.edu>
2021-04-23 15:06:37 -0700

    Create tiles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,8 +13,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x += 0.1f;
+        position.x += 3.0f * horizontal * Time.deltaTime;
+        position.y += 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
